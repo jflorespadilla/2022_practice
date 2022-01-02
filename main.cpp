a53d24a9fa316ebd62039ffdc37f936e32ccfb44
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2022-01-01 20:31:38 -0800

    First commit of the project. Declare goals and ambitions for the year.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
+void greetings();
+
 int main() {
-	std::cout << "Mis nopales!" << std::endl;
+	greetings();
 
 	system("PAUSE");
 	
 	return 0;
+}
+
+void greetings() {
+	std::cout << "Hello and welcome to 2022! This is where I'll go from zero to programming hero!" << std::endl;
+	std::cout << "My goal is to program everyday this year. While it doesn't have to be this project, I do want this to serve as my go-to program to messaround in." << std::endl;
+	std::cout << "Simply hope is to get my career back on track and make my way into some sort of development role somewhere." << std::endl;
 }
