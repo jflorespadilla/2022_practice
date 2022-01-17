74a281f24dde5d8ce26e65e2c0bacc08049ef33e
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2022-01-16 22:06:39 -0800

    Created a simple character create class. Still going over smart pointer documentation in order to build a competent Game class.

--- source/Game.cpp
+++ source/Game.cpp
@@ -9,6 +9,10 @@ Game::~Game() {
 }
 
 void Game::run() {
+	if (character.get() == NULL) {
+		createCharacter();
+	}
+
 	char input;
 	while (!quit) {
 		std::cout << "Game is running. Press Q to Quit" << std::endl << std::endl;
@@ -17,4 +21,8 @@ void Game::run() {
 			quit = true;
 		}
 	}
+}
+
+void Game::createCharacter() {
+	std::cout << "This is the create character function" << std::endl << std::endl;
 }
