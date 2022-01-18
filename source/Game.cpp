fe22d188dfc3abcd06b680664e521138391cbe23
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2022-01-17 23:23:31 -0800

    Created a simple character creator function. This was a quick and dirty implementiation. I need to see fi there is a cleaner way of doing this.

--- source/Game.cpp
+++ source/Game.cpp
@@ -15,6 +15,8 @@ void Game::run() {
 
 	char input;
 	while (!quit) {
+		std::cout << "Your character is " << character->getName() << std::endl <<std::endl; 
+
 		std::cout << "Game is running. Press Q to Quit" << std::endl << std::endl;
 		std::cin >> input;
 		if (input == 'q' || input == 'Q') {
@@ -25,4 +27,20 @@ void Game::run() {
 
 void Game::createCharacter() {
 	std::cout << "This is the create character function" << std::endl << std::endl;
+	std::string name, ability;
+	int health, defense, attackDamage;
+
+	std::cout << "Create your character!\nName: ";
+	std::cin >> name;
+	std::cout << "Ability: ";
+	std::cin >> ability;
+	std::cout << "Health: ";
+	std::cin >> health;
+	std::cout << "Defense: ";
+	std::cin >> defense;
+	std::cout << "Attack Damage: ";
+	std::cin >> attackDamage;
+
+	std::cout << "Creating Hero Character..." << std::endl << std::endl;
+	character.reset(new hero(name, ability, health, defense, attackDamage));
 }
