2d36099b814a071ad7287259f9a16c553b61bbc8
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2022-02-22 23:55:28 -0800

    Started working on map coordinate functions. Need to make design choices about how maps work and are generated.

--- headers/GameMap.h
+++ headers/GameMap.h
@@ -1,13 +1,22 @@
 #pragma once
+#include <vector>
 
 enum Terrain_Type {sand, stone, grass, swamp, marsh, forrest, jungle, mountain};
 
+struct CoordinateCell {
+	int x, y;
+	Terrain_Type terrain;
+};
+
 class GameMap {
 	public:
-		
+		GameMap(int x=10, int y=10);
+		~GameMap();
+		int getArea();
+
 	private:
 		int yMax, xMax;
-		// Figure out how to integrate the terrain type to this class
+		std::vector<CoordinateCell> coordinateCells;
 };
 
 
--- source/GameMap.cpp
+++ source/GameMap.cpp
@@ -1,1 +1,17 @@
-#include "GameMap.h"
+#include "..\headers\GameMap.h"
+
+GameMap::GameMap(int x, int y) {
+    yMax = y;
+    xMax = x;
+    coordinateCells.resize(yMax * xMax);
+    
+    // Generate Default Map
+}
+
+GameMap::~GameMap() {
+    
+}
+
+int GameMap::getArea() {
+    return yMax * xMax;
+}
