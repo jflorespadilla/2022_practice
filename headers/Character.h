b24b5b89490afba4c4d742b2b977a28f70d281c8
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2022-04-16 20:27:17 -0700

    Made the Character class a pure virtual class. I am going to experiment with the results a bit and see if I can get some learning done.

--- headers/Character.h
+++ headers/Character.h
@@ -4,16 +4,16 @@
 
 class Character {
 public:
-	virtual std::string getName() { return name; };
-	virtual std::string getAbility() { return ability; };
-	virtual int getHealth() { return health; };
-	virtual int getAttackDamage() { return attackDamage; };
+	virtual std::string getName() = 0;
+	virtual std::string getAbility() = 0;
+	virtual int getHealth() = 0;
+	virtual int getAttackDamage() = 0;
 
-	virtual void setName(std::string nameVal) { name = nameVal; };
-	virtual void setAbility(std::string abilityVal) { ability = abilityVal; };
-	virtual void setHealth(int healthVal) { health = healthVal; };
-	virtual void setAttackDamage(int attackDamageVal) { attackDamage = attackDamageVal; };
-	virtual bool checkIfAlive() { return true; };
+	virtual void setName(std::string nameVal) = 0;
+	virtual void setAbility(std::string abilityVal) = 0;
+	virtual void setHealth(int healthVal) = 0;
+	virtual void setAttackDamage(int attackDamageVal) = 0;
+	virtual bool checkIfAlive() = 0;
 
 private:
 	std::string name;
