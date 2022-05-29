0ed2266b301141c1912d9e0ff4be97cf8b5815e5
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2022-05-29 15:32:44 -0700

    Still psudo coding.

--- source/InputManager.cpp
+++ source/InputManager.cpp
@@ -11,8 +11,11 @@ InputManager::~InputManager() {
 
 char InputManager::GetKey() {
 	// set the input handle
-
 	// Iterate over events
+	for (int i = 0; i < 256; i++) {
+		// from what I see online, I need to iterate over 256 keys.
+		// I hope more reading clears up some of the confusion I have over this.
+	}
 		// Store key states
 	// Return released keys
 	return 0; // Stub return
