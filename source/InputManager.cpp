5c1786eac075f1552915ce9b92e4fbe6d427d4b0
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2022-05-30 21:58:28 -0700

    Marking for a late night coding session.

--- source/InputManager.cpp
+++ source/InputManager.cpp
@@ -12,6 +12,9 @@ InputManager::~InputManager() {
 char InputManager::GetKey() {
 	// set the input handle
 	// Iterate over events
+
+
+	// Late night code marker
 	for (int i = 0; i < 256; i++) {
 		// from what I see online, I need to iterate over 256 keys.
 		// I hope more reading clears up some of the confusion I have over this.
