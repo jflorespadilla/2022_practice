d7b960ad06de604e2858e3f11aadf2ea8fe1ab86
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2022-06-07 01:29:47 -0700

    Got rid of the comments. Going to do a late night code push.

--- source/InputManager.cpp
+++ source/InputManager.cpp
@@ -10,20 +10,12 @@ InputManager::~InputManager() {
 }
 
 char InputManager::GetKey() {
-	// set the input handle
-	// Iterate over events
 
-
-	// Late night code marker
 	for (int i = 0; i < 256; i++) {
-		// from what I see online, I need to iterate over 256 keys.
-		// I hope more reading clears up some of the confusion I have over this.
 
-		// Having a hard time coding. Another empty push.
 	}
-		// Store key states
-	// Return released keys
-	return 0; // Stub return
+
+	return 0;
 }
 
 
