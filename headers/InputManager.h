de1a08cb6955be6c7c7ecf9c00e506ec8186097f
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2022-06-08 23:21:23 -0700

    Strated writing the GetLine() function.

--- headers/InputManager.h
+++ headers/InputManager.h
@@ -1,12 +1,14 @@
 #pragma once
 #include <Windows.h>
 #include <iostream>
+#include <string>
 
 class InputManager{
 public:
 	InputManager(HANDLE* hStdin);
 	~InputManager();
 	char GetKey();
+	std::string GetLine();
 
 private:
 	HANDLE* _hStdin;
--- source/InputManager.cpp
+++ source/InputManager.cpp
@@ -12,12 +12,19 @@ char InputManager::GetKey() {
 	INPUT_RECORD irInBuf[265];
 	DWORD recordsRead;
 	ReadConsoleInput(*_hStdin, irInBuf, 256, &recordsRead);
-	// It seems to be working, but I need to do more testing.
+
 	for (DWORD i = 0; i < recordsRead; i++) {
 		if (irInBuf[i].Event.KeyEvent.bKeyDown) {
 			return irInBuf[i].Event.KeyEvent.uChar.AsciiChar;
 		}
 	}
 }
+
+std::string InputManager::GetLine() {
+	// Read input per line
+	return std::string();
+}
+
+
 
 
