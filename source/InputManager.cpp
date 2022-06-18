0a0c6d3966b3285f044a7530fe44ab2113972539
Jesus Flores-Padilla <jflorespadilla@nevada.unr.edu>
2022-06-18 12:22:36 -0700

    Psudo code update.

--- source/InputManager.cpp
+++ source/InputManager.cpp
@@ -32,7 +32,8 @@ std::string InputManager::GetLine() {
 	
 	for (DWORD i = 0; i < recordsRead; i++) {
 		if (!irInBuf[i].Event.KeyEvent.bKeyDown) {
-			// Reviewing docs
+			// Going to rework the method to read the records.
+			// I'll need to see how some polling methods might work.
 		}
 	}
 
